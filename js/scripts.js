1a0e60164d15cec1a1420e89d394f50dbe4d5434
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-02-24 10:23:24 +0300

    FInal commit

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,21 @@ $(document).ready(function(){
         $(this).children(".hide").toggle(1200);
         $(this).children(".show").toggle(1500);
     });
+    // giving feedback to the customer
+$("button").click(function (feedback) {
+    var client = document.getElementById('name').value;
+    alert(' Dear ' + ' esteemed ' + client + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
+    feedback.preventDefault();
+  });
+  //portifolio section 
+$(".card").hover(function () {
+    $(this).children(".c").fadeToggle(1000);
+  });
 });
+
+  //    reset my form button
+$("button").on('click' ,function () {
+    $('form').each(function () {
+      this.reset();
+    });
+  });
