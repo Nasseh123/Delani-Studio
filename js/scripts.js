7a6a4102da10a8f17049187cf3efc4c14a6f870d
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-02-24 21:11:37 +0300

    CHanged the contact form function to blend with the api function.It was achieved by removing the default code to enable the api to gather the information inputed

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-    $(".clickable").click(function(){
+    $(".clickable").hover(function(){
         $(this).children(".hide").toggle(1200);
         $(this).children(".show").toggle(1500);
     });
     // giving feedback to the customer
-$("button").click(function (feedback) {
-    var client = document.getElementById('name').value;
+$(".button").click(function (feedback) {
+    var client = document.getElementById('mce-FNAME').value;
     alert(' Dear ' + ' esteemed ' + client + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
-    feedback.preventDefault();
+  
   });
   //portifolio section 
 $(".card").hover(function () {
