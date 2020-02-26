a6392c2efa75d7bc33b8841cdfa65ec0237c26a0
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-02-26 10:51:23 +0300

    Addition of email alert in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//What we do section.
 $(document).ready(function(){
     $(".clickable").click(function(){
         $(this).children(".hide").toggle(1200);
@@ -6,7 +7,7 @@ $(document).ready(function(){
     // giving feedback to the customer
 $(".button").click(function (feedback) {
     var client = document.getElementById('mce-FNAME').value;
-    alert(' Dear ' + ' esteemed ' + client + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
+    alert(' Dear ' + ' esteemed ' + client + ' we have received your message successful. ' + ' Feel free to reach out to us anytime'+'\n \ndYou will be contacted to your email for a welcome note!');
   
   });
   //portifolio section 
